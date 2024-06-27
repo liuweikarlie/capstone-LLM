b79695995e98e0382114018b3251bdcada6f6314
liuweikarlie <58883510+liuweikarlie@users.noreply.github.com>
2024-06-27 11:17:37 +0800

    rag-retrieve

---
+++ RAG/Retrieve/retrieve.py
@@ -0,0 +1,61 @@
+import uuid
+
+from langchain.retrievers.multi_vector import MultiVectorRetriever
+from langchain.storage import InMemoryStore
+from langchain_community.vectorstores import Chroma
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+
+
+def create_multi_vector_retriever(
+    vectorstore, text_summaries, texts
+):
+    """
+    Create retriever that indexes summaries, but returns raw images or texts
+    """
+
+    # Initialize the storage layer
+    store = InMemoryStore()
+    id_key = "doc_id"
+
+    # Create the multi-vector retriever
+    retriever = MultiVectorRetriever(
+        vectorstore=vectorstore,
+        docstore=store,
+        id_key=id_key,
+    )
+
+    # Helper function to add documents to the vectorstore and docstore
+    def add_documents(retriever, doc_summaries, doc_contents):
+        doc_ids = [str(uuid.uuid4()) for _ in doc_contents]
+        summary_docs = [
+            Document(page_content=s, metadata={id_key: doc_ids[i]})
+            for i, s in enumerate(doc_summaries)
+        ]
+        retriever.vectorstore.add_documents(summary_docs)
+        retriever.docstore.mset(list(zip(doc_ids, doc_contents)))
+
+    # Add texts, tables, and images
+    # Check that text_summaries is not empty before adding
+    if text_summaries:
+        add_documents(retriever, text_summaries, texts)
+   
+    return retriever
+
+
+def retrieve_final(text_summaries, texts):
+# The vectorstore to use to index the summaries
+    vectorstore = Chroma(
+        collection_name="mm_rag_cj_blog", embedding_function=OpenAIEmbeddings()
+    )
+
+    # Create retriever
+    retriever_multi_vector_img = create_multi_vector_retriever(
+        vectorstore,
+        text_summaries,
+        texts,
+    
+    )
+
+
+    
